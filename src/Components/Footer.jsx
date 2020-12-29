393b211b5883a4ed0c3e3fa43089e7cfc1d96032
Muhammad Khashaish Rabbani <60380153+mkrabbani@users.noreply.github.com>
2020-12-30 04:37:54 +0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Box, ChakraProvider, Divider, Text, Stack, Image, Container, Heading, Link, Flex } from '@chakra-ui/react';
 
+
 const Footer = () => {
     return (
         <ChakraProvider>
@@ -72,7 +73,7 @@ const Footer = () => {
                     <Box mb={10} >
                         <Stack direction={['column','column','row','row']} spacing={['','','','117px']}>
                             <Box>
-                                <Image src="/zen-zero-effort-grey.png" />
+                                <Image src="zen-zero-effort-grey.png" />
                                 <Box maxW="900px" mt={5}>
                                     <Text fontSize="12px" color="#AAAAAA">ZEN.COM is a licensed financial institution under the supervision of the Central Bank of Lithuania approved by European Banking Authority for 31 countries. Registration number of the company 304749651, VAT ID LT100011714916. License of electronic money institutions number LB000457. Share capital 2.185.000,00 EUR. Company address: Mėsinių g. 5, LT-01133, Vilnius, Lithuania. Sales Office: ul. Emilii Plater 53, 00-113 Warsaw, Poland.</Text>
                                 </Box>
